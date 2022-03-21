895dc84c43058a8791b029c0f7c9504a91567149
Sergei Terentev <sergei.terentev@aspose.com>
2022-03-21 09:27:25 +0300

    Update examples for v22.3

--- Examples/src/main/java/examples/Convert/ConvertToPdfDirectOptions.java
+++ Examples/src/main/java/examples/Convert/ConvertToPdfDirectOptions.java
@@ -1,6 +1,7 @@
 package examples.Convert;
 
 import com.groupdocs.cloud.conversion.client.*;
+import com.groupdocs.cloud.conversion.model.*;
 import com.groupdocs.cloud.conversion.model.requests.*;
 import com.groupdocs.cloud.conversion.api.*;
 import examples.Constants;
@@ -9,16 +10,19 @@ import java.io.File;
 /**
  * This example demonstrates how to convert document into pdf without using cloud storage
  */
-public class ConvertToPdfDirect {
+public class ConvertToPdfDirectOptions {
 
 	public static void main(String[] args) {
 		try {
 			// Create API instance
 			ConvertApi apiInstance = new ConvertApi(Constants.GetConfiguration());
 
-			File file = new File("examples\\src\\main\\resources\\WordProcessing\\four-pages.docx");
+			File file = new File("examples\\src\\main\\resources\\WordProcessing\\password-protected.docx");
+			DocxLoadOptions loadOptions = new DocxLoadOptions();
+			loadOptions.setFormat("docx");
+			loadOptions.setPassword("password");
 
-			ConvertDocumentDirectRequest request = new ConvertDocumentDirectRequest("pdf", file, 1, 0, null, null); // all pages
+			ConvertDocumentDirectRequest request = new ConvertDocumentDirectRequest("pdf", file, 1, 0, loadOptions, null); // all pages
 
 			File result = apiInstance.convertDocumentDirect(request);
 
