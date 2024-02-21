e6e07778ecba75363615b66533f1198c1378e745
Sergei Terentev <sergei.terentev@aspose.com>
2024-02-21 08:36:10 +0300

    Update examples for v24.2

--- Examples/src/main/java/examples/Convert/ConvertToPdf.java
+++ Examples/src/main/java/examples/Convert/ConvertToPdf.java
@@ -31,7 +31,7 @@ public class ConvertToPdf {
 			convertOptions.setCenterWindow(true);
 			convertOptions.setCompressImages(false);
 			convertOptions.setDisplayDocTitle(true);
-			convertOptions.setDpi(1024.0);
+			convertOptions.setDpi(1024);
 			convertOptions.setFitWindow(false);
 			convertOptions.setFromPage(1);
 			convertOptions.setGrayscale(false);
